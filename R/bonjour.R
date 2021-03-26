5eebc694b33dcd47ee8e81fe059ccca421c16389
Maëlle Salmon <maelle.salmon@yahoo.se>
2021-03-26 08:58:38 +0100

    Initial commit

---
+++ R/bonjour.R
@@ -0,0 +1,10 @@
+#' Bonjour
+#'
+#' @return A polite greeting
+#' @export
+#'
+#' @examples
+#' bonjour()
+bonjour <- function() {
+  "Bonjour tout le monde !"
+}
---
+++ inst/make-logo.R
@@ -0,0 +1,14 @@
+imgurl <- "inst/1616743924914.jpg"
+
+hexSticker::sticker(
+  imgurl,
+  package = "Informalprez",
+  p_size = 13,
+  s_x = 1,
+  s_y = .75,
+  s_width = .6,
+  p_color = "#d7cba5",
+  h_color = "#d7cba5",
+  h_fill = "#1e5f9c",
+  filename ="inst/logo.png"
+)
